b8e74139afe14bd3db4f7e68dafe21261f7d649a
Abiodun Olanlokun <abtech247@gmail.com>
2022-08-01 08:20:25 +0100

    wip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'https://v2.api.ibrolinks.com/api/',
+  url: 'https://trade-depot-be.herokuapp.com/api/',
   flutterwave_pubkey: 'FLWPUBK-aabfdcc654e329a5c55c2971facc9cfe-X',
   monnify_contract: '344391255877',
   monnify_key: 'MK_PROD_LPW6S6BFU6'
